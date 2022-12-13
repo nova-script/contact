d8000a5a42ae8a965e39db2f3c5e5b5068874e3f
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-13 11:46:24 -0300

    :bulb: Add prisma files

---
+++ notifications/prisma/migrations/20221213135104_create_notifications/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "content" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "recipientId" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE INDEX "Notification_recipientId_idx" ON "Notification"("recipientId");
